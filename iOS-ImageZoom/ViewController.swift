adcd2b934d7d68ff1307bbe4704e7276af455af5
Deepak <deepak.kadarivel@gmail.com>
2016-01-28 16:55:48 +0530

    initial Commit

--- iOS-ImageZoom/ViewController.swift
+++ iOS-ImageZoom/ViewController.swift
@@ -8,18 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        scrollView.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
