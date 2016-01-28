30b653aab494a9516f6dde11f9a15e49e8fb8858
Deepak <deepak.kadarivel@gmail.com>
2016-01-28 17:53:04 +0530

    CenterScrollViewContent method

--- iOS-ImageZoom/ViewController.swift
+++ iOS-ImageZoom/ViewController.swift
@@ -54,7 +54,28 @@ class ViewController: UIViewController, UIScrollViewDelegate, UIImagePickerContr
         scrollView.maximumZoomScale = 1
         scrollView.zoomScale = minScale
         
+        self.centerContentSize()
+        
         picker.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func centerContentSize() {
+        let boundSize = scrollView.bounds.size
+        var contentsFrame = imageView.frame
+        
+        if contentsFrame.size.width < boundSize.width {
+            contentsFrame.origin.x = (boundSize.width - contentsFrame.size.width) / 2
+        } else {
+            contentsFrame.origin.x = 0
+        }
+        
+        if contentsFrame.size.height < boundSize.height {
+            contentsFrame.origin.y = (boundSize.height - contentsFrame.size.height) / 2
+        } else {
+            contentsFrame.origin.y = 0
+        }
+        
+        imageView.frame = contentsFrame
     }
 
 }
