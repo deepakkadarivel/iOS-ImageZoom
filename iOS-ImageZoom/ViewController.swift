7164fe84ad041f9a2818dcbf95a7a550b9797fe3
Deepak <deepak.kadarivel@gmail.com>
2016-01-28 18:51:25 +0530

    ImageZoom complete

--- iOS-ImageZoom/ViewController.swift
+++ iOS-ImageZoom/ViewController.swift
@@ -12,27 +12,19 @@ class ViewController: UIViewController, UIScrollViewDelegate, UIImagePickerContr
 
     @IBOutlet weak var scrollView: UIScrollView!
     
-    @IBOutlet weak var imageView: UIImageView!
+    var imageView = UIImageView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         scrollView.delegate = self
+        
+        imageView.frame = CGRectMake(0, 0, scrollView.frame.size.width, scrollView.frame.size.height)
+        imageView.userInteractionEnabled = true
+        scrollView.addSubview(imageView)
     }
     
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(true)
-        let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "openGallery")
-        self.navigationItem.rightBarButtonItem = addButton
-        self.navigationController?.navigationBar.topItem?.title = "ImageZoom"
-        self.navigationController?.navigationBar.tintColor = UIColor.whiteColor()
-        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
-        self.navigationController?.navigationBar.translucent = true
-        self.navigationController?.navigationBar.barTintColor = UIColor.clearColor()
-        self.navigationController?.view.backgroundColor = UIColor.clearColor()
-    }
-    
-    func openGallery() {
+    @IBAction func openGallery(sender: AnyObject) {
         let imagePicker = UIImagePickerController()
         imagePicker.delegate = self
         imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
@@ -42,6 +34,8 @@ class ViewController: UIViewController, UIScrollViewDelegate, UIImagePickerContr
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
         let image = info[UIImagePickerControllerOriginalImage] as! UIImage
         imageView.image = image
+        imageView.contentMode = UIViewContentMode.Center
+        imageView.frame = CGRectMake(0, 0, image.size.width, image.size.height)
         
         scrollView.contentSize = image.size
         
@@ -76,6 +70,14 @@ class ViewController: UIViewController, UIScrollViewDelegate, UIImagePickerContr
         }
         
         imageView.frame = contentsFrame
+    }
+    
+    func scrollViewDidZoom(scrollView: UIScrollView) {
+        self.centerContentSize()
+    }
+    
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return imageView
     }
 
 }
