7f5c37368f69e4a89bdde0ab2c5c474c010862fa
Deepak <deepak.kadarivel@gmail.com>
2016-01-28 17:28:53 +0530

    ImagePicker and ImageView

--- iOS-ImageZoom/ViewController.swift
+++ iOS-ImageZoom/ViewController.swift
@@ -12,10 +12,37 @@ class ViewController: UIViewController, UIScrollViewDelegate, UIImagePickerContr
 
     @IBOutlet weak var scrollView: UIScrollView!
     
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         scrollView.delegate = self
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(true)
+        let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "openGallery")
+        self.navigationItem.rightBarButtonItem = addButton
+        self.navigationController?.navigationBar.topItem?.title = "ImageZoom"
+        self.navigationController?.navigationBar.tintColor = UIColor.whiteColor()
+        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+        self.navigationController?.navigationBar.translucent = true
+        self.navigationController?.navigationBar.barTintColor = UIColor.clearColor()
+        self.navigationController?.view.backgroundColor = UIColor.clearColor()
+    }
+    
+    func openGallery() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        self.presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        imageView.image = image
     }
 
 }
